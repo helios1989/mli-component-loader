1e99776b048f567b73d02831b374dc00e770c07e
Vergel Barit <baritve@mliph.manulife.com.ph>
2018-11-12 16:00:10 +0800

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,18 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-
 import { AppComponent } from './app.component';
-
+import { CoreProfileModule } from './ui-modules/profile/profile.module';
+import { MliComponentLoaderModule } from './shared/mli-component-loader/mli-component-loader.module';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    MliComponentLoaderModule,
+    CoreProfileModule
   ],
   providers: [],
   bootstrap: [AppComponent]
